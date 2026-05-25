1428a775713d091a811d380e927d30ff130f75a7
Vanessa Soares <vanessa.soares@aluno.cefet-rj.br>
2026-05-25 14:23:20 +0000

    update config

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,5 +1,8 @@
+
 export const API_URL = 'http://localhost:5000';
 //export const API_URL = 'http://aquarii.eic.cefet-rj.br/quintana-api'
 export const APP_MODE = process.env.NEXT_PUBLIC_APP_MODE || 'demo';
 export const IS_WORKSHOP_MODE = APP_MODE === 'workshop';
 export const APP_VERSION = process.env.NEXT_PUBLIC_APP_VERSION || '1.0.0';
+
+
